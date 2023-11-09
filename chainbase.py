c5161e08c5c3f22737e798ef872520637286ec0a
kai gouthro <777603+kaigouthro@users.noreply.github.com>
2023-11-08 23:14:07 -0500

    Create chainbase.py

---
+++ chainbase.py
@@ -0,0 +1,50 @@
+from langchain import LangChain
+from langchain.laapi import LLM  # We'll use Langchain's built-in LLM agent
+from langchain.agents import Agent  # Import the base Langchain agent class
+from langchain.utilities import save_as_markdown
+from langchain.saved_prompts import PromptLibrary
+
+# We first create a Langchain instance
+lc = LangChain()
+
+# Defining an agent for web scraping
+class WebScraperAgent(Agent):
+    def scrape_and_save_as_markdown(self, search_query):
+        search_results = lc.ask(search_query, agent="search_engine")  # Use the search engine agent
+        markdown_data = save_as_markdown(search_results['text'])
+        file_path = f"{search_query.replace(' ', '_')}.md"
+        with open(file_path, 'w') as file:
+            file.write(markdown_data)
+
+# Defining an agent for code refactoring and rewriting
+class CodeRefactorerAgent(Agent):
+    def refactor_and_rewrite_code(self, code):
+        refactoring_instructions = lc.get_prompt("Refactor code", {"code": code}, [], prompt_library=PromptLibrary())
+        refactored_code = lc.ask(refactoring_instructions, agent=LLM.queue)
+        
+        # Assuming that refactored_code is a dictionary with the key 'text' containing the result
+        rewriting_instructions = lc.get_prompt("Rewrite code", {"code": refactored_code['text']}, [], prompt_library=PromptLibrary())
+        rewritten_code = lc.ask(rewriting_instructions, agent=LLM.queue)
+        
+        return rewritten_code['text']
+
+# Defining the main assistant using the Langchain tools
+class LangchainAssistant:
+    def __init__(self):
+        self.web_scraper = WebScraperAgent()
+        self.code_refactorer = CodeRefactorerAgent()
+
+    def scrape_web_and_convert_to_markdown(self, search_query):
+        self.web_scraper.scrape_and_save_as_markdown(search_query)
+
+    def refactor_and_rewrite_code(self, code):
+        return self.code_refactorer.refactor_and_rewrite_code(code)
+
+# Utilizing the Langchain Assistant in action
+# This would be an example of how to use the Assistant
+assistant = LangchainAssistant()
+markdown_result = assistant.scrape_web_and_convert_to_markdown("Langchain documentation")
+print(markdown_result)
+
+rewritten_code = assistant.refactor_and_rewrite_code("# Some example Python code that needs refactoring")
+print(rewritten_code)
