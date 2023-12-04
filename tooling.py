a1e6b8481091c574e81d2ba1ea1ae8465630649e
kai gouthro <777603+kaigouthro@users.noreply.github.com>
2023-12-04 08:09:38 -0700

    check

---
+++ tooling.py
@@ -0,0 +1,387 @@
+import git
+import networkx
+
+
+class Architecture:
+    """
+    Architecture
+
+    a tree structure holding clas and function definitions and attributes,
+    with input and output variables and current state, also what task they are asssigned to be built by
+    and a basic networkx node/edge implementation for calls and control flow
+
+    - planned
+    - in progress
+    - complete
+    - passing
+    - failing
+
+    """
+    def __init__(self):
+        self.schematic = {}
+        self.parent = None
+        self.state = "planned"
+        self.inputs = None
+        self.outputs = None
+        self.network = networkx
+
+
+class _GitApi:
+    def __init__(self, username, token):
+        self.token = token
+        self.user = username
+        self.client = self.set_client()
+        self.repository_info = {}
+
+    def set_client(self, address="https://api.github.com"):
+        self.client = git.Git(address)
+
+    @staticmethod
+    def get_repository(repository_id):
+        """
+        Get a GitHub repository
+
+        Args:
+            repository_id (int): The ID of the repository
+
+        Returns:
+            repository (dict): The repository information
+                - id (int): The ID of the repository
+                - name (str): The name of the repository
+                - description (str): The description of the repository
+                - owner (str): The owner of the repository
+                - url (str): The URL of the repository
+        """
+
+    @staticmethod
+    def create_issue(repository_id, title, body):
+        """
+        Create a new GitHub issue
+
+        Args:
+            repository_id (int): The ID of the repository
+            title (str): The title of the issue
+            body (str): The body of the issue
+
+        Returns:
+            issue (dict): The issue information
+                - id (int): The ID of the issue
+                - title (str): The title of the issue
+                - body (str): The body of the issue
+                - url (str): The URL of the issue
+        """
+
+    @staticmethod
+    def get_pull_requests(repository_id):
+        """
+        Get pull requests from a GitHub repository
+
+        Args:
+            repository_id (int): The ID of the repository
+
+        Returns:
+            pull_request (dict): The pull request information
+                - id (int): The ID of the pull request
+                - title (str): The title of the pull request
+                - body (str): The body of the pull request
+                - url (str): The URL of the pull request
+        """
+
+    @staticmethod
+    def create_pull_request(repository_id, title, body):
+        """
+        Create a new GitHub pull request
+
+        Args:
+            repository_id (int): The ID of the repository
+            title (str): The title of the pull request
+            body (str): The body of the pull request
+
+        Returns:
+            pull_request (dict): The pull request information
+                - id (int): The ID of the pull request
+                - title (str): The title of the pull request
+                - body (str): The body of the pull request
+                - url (str): The URL of the pull request
+        """
+
+    @staticmethod
+    def get_file_contents(repository_id, path):
+        """
+        Get contents of a file from a GitHub repository
+
+        Args:
+            repository_id (int): The ID of the repository
+            path (str): The path of the file
+
+        Returns:
+            file_contents (dict): The file contents information
+                - path (str): The path of the file
+                - content (str): The content of the file
+                - sha (str): The SHA of the file
+        """
+
+    @staticmethod
+    def update_file_contents(repository_id, path, content, commit_message):
+        """
+        Update contents of a file in a GitHub repository
+
+        Args:
+            repository_id (int): The ID of the repository
+            path (str): The path of the file
+            content (str): The new content of the file
+            commit_message (str): The commit message
+
+        Returns:
+            commit (dict): The commit information
+                - message (str): The commit message
+                - tree (str): The tree of the committed changes
+                - parents (str): The parent commits of the commit
+        """
+
+    @staticmethod
+    def push_changes(repository_id, branch):
+        """
+        Push changes to a GitHub repository
+
+        Args:
+            repository_id (int): The ID of the repository
+            branch (str): The branch to push the changes to
+
+        Returns:
+            push (dict): The push information
+                - branch (str): The branch that was pushed
+        """
+
+    @staticmethod
+    def create_repository(name):
+        """
+        Create a new GitHub repository
+
+        Args:
+            name (str): The name of the repository
+
+        Returns:
+            repository (dict): The repository information
+                - id (int): The ID of the repository
+                - name (str): The name of the repository
+                - url (str): The URL of the repository
+        """
+
+    @staticmethod
+    def authenticate_as_app_installation(installation_id):
+        """
+        Authenticate as an app installation on GitHub
+
+        Args:
+            installation_id (int): The ID of the app installation
+
+        Returns:
+            authorization (dict): The authorization information
+                - token (str): The authentication token
+                - expires_at (str): The expiration date of the token
+        """
+
+    @staticmethod
+    def create_issue_as_authenticated_installation(installation_id, repository_id, title, body):
+        """
+        Create a new GitHub issue using an authenticated installation
+
+        Args:
+            installation_id (int): The ID of the app installation
+            repository_id (int): The ID of the repository
+            title (str): The title of the issue
+            body (str): The body of the issue
+
+        Returns:
+            issue (dict): The issue information
+                - id (int): The ID of the issue
+                - title (str): The title of the issue
+                - body (str): The body of the issue
+                - url (str): The URL of the issue
+        """
+
+    @staticmethod
+    def create_branch_here(branchname):
+        """
+        Create a new branch in a GitHub repository
+
+        Args:
+            branchname (str): The name of the branch
+
+        Returns:
+            branch (dict): The branch information
+                - name (str): The name of the branch
+                - commit (dict): The commit information
+                    - message (str): The commit message
+                    - tree (str): The tree of the committed changes
+                    - parents (str): The parent commits of the commit
+        """
+
+        # Implementation details...
+
+
+class TaskStorage:
+    def __init__(self):
+        self.tasks = []
+        self.variables = {}  # {"uuid": {"name": "value",}}
+
+    @staticmethod
+    def add_task(name, task_id, parent_id, child_ids):
+        """
+        Add a task to the storage
+
+        Args:
+            name (str): The name of the task
+            task_id (int): The ID of the task
+            parent_id (int): The ID of the parent task
+            child_ids (list): The IDs of the child tasks
+
+        Returns:
+            output (str): The output string after adding the task
+        """
+        # Implementation details...
+
+    @staticmethod
+    def remove_task(task_id):
+        """
+        Remove a task from the storage
+
+        Args:
+            task_id (int): The ID of the task
+
+        Returns:
+            output (str): The output string after removing the task
+        """
+        # Implementation details...
+
+    @staticmethod
+    def get_all_tasks():
+        """
+        Get all tasks from the storage
+
+        Returns:
+            output (str): The output string containing all tasks
+        """
+        # Implementation details...
+
+    @staticmethod
+    def get_task_by_id(task_id):
+        """
+        Get a task by its ID from the storage
+
+        Args:
+            task_id (int): The ID of the task
+
+        Returns:
+            output (str): The output string containing the task with the specified ID
+        """
+        # Implementation details...
+
+    @staticmethod
+    def get_tasks_by_parent_id(parent_id):
+        """
+        Get tasks by their parent ID from the storage
+
+        Args:
+            parent_id (int): The ID of the parent task
+
+        Returns:
+            output (str): The output string containing the tasks with the specified parent ID
+        """
+        # Implementation details...
+
+    @staticmethod
+    def get_parent_by_child_id(child_id):
+        """
+        Get parent by their child ID from the storage
+
+        Args:
+            child_id (int): The ID of the child task
+
+        Returns:
+            output (str): The output string containing the parent with the specified child ID
+        """
+        # Implementation details...
+
+    @staticmethod
+    def set_instructions(task_id, instructions):
+        """
+        Set instructions for a task
+
+        Args:
+            task_id (int): The ID of the task
+            instructions (str): The instructions for the task
+
+        Returns:
+            output (str): The output string after setting the instructions
+        """
+        # Implementation details...
+
+    @staticmethod
+    def get_instructions(task_id):
+        """
+        Get instructions for a task
+
+        Args:
+            task_id (int): The ID of the task
+
+        Returns:
+            output (str): The output string containing the instructions for the task
+        """
+        # Implementation details...
+
+    @staticmethod
+    def get_unfinished_tasks():
+        """
+        Get all unfinished tasks
+
+        Returns:
+            output (str): The output string containing all unfinished tasks
+        """
+        # Implementation details...
+
+    @staticmethod
+    def get_finished_tasks():
+        """
+        Get all finished tasks
+
+        Returns:
+            output (str): The output string containing all finished tasks
+        """
+        # Implementation details...
+
+    @staticmethod
+    def modify_sequence(sequence):
+        """
+        Modify the execution sequence of the tasks
+
+        Args:
+            sequence (list): The list of tasks in the execution sequence
+
+        Returns:
+            output (str): The output string after modifying the sequence
+        """
+        # Implementation details...
+
+    @staticmethod
+    def get_sequence():
+        """
+        Get the execution sequence of the tasks
+
+        Returns:
+            output (list): The list of tasks in the execution sequence
+        """
+        # Implementation details...
+
+    @classmethod
+    def add_from_list(cls, task_list):
+        """
+        Add tasks from a List to the TaskStorage
+
+        Args:
+            task_list (list): The list of tasks to add
+        """
+
+        for task in task_list:
+            cls.add_task(task["name"], task["task_id"], task["parent_id"], task["child_ids"])
